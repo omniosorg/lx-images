c9146282439bb98a91b32b3c2f9339c7ba8f5943
Tobias Oetiker <tobi@oetiker.ch>
2020-06-13 15:01:51 +0200

    make user@.service work
    services with `User=` fail to launch because 
    systemd does not seem to realize that /dev/null is NOT a terminal
    under lx but when trying to chown it, it fails and thus the `User=`
    directive does not work properly ... this little hack fixes the
    behavior for the user@.service but obviously it has to be fixed in lx :)

--- ubuntu/helpers/build.sh
+++ ubuntu/helpers/build.sh
@@ -17,6 +17,7 @@ apt-get install -yq \
     lsb-release \
     less \
     joe \
+    man-db \
     net-tools
 apt-get -qq clean
 apt-get -qq autoremove 
@@ -24,14 +25,22 @@ apt-get -qq autoremove
 # disable services we do not need
 systemctl disable systemd-resolved fstrim.timer fstrim
 if [ $UBUNTU = 20.04 ]; then
-    systemctl disable e2scrub_all e2scrub_all.timer
+    systemctl disable e2scrub_reap e2scrub_all e2scrub_all.timer
+    # systemd does not seem to realize that /dev/null is NOT a terminal
+    # under lx but when trying to chown it, it fails and thus the `User=`
+    # directive does not work properly ... this little trick fixes the
+    # behavior for the user@.service but obviously it has to be fixed in
+    # lx :) ...
+    touch /etc/empty
+    mkdir -p /etc/systemd/system/user@.service.d
+    echo StandardInput=file:/etc/empty > /etc/systemd/system/user@.service.d/override.conf
 fi
 
 # disable systemd faeatures not present in lx (namely cgroup support)
 for S in systemd-hostnamed systemd-localed systemd-timedated systemd-logind systemd-initctl  systemd-journald; do
   O=/etc/systemd/system/${S}.service.d
   mkdir -p $O
-  cp override.conf ${O}/override.conf
+  cp override.conf ${O}/override.conf  
 done
  
 # Prevents apt-get upgrade issue when upgrading in a container environment.
