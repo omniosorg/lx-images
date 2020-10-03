516c6c74278eba8907c5c160dcadca7a908711d0
Andy Fiddaman <omnios@citrus-it.co.uk>
2020-10-03 16:25:28 +0100

    Add debian buster image

--- debian/helpers/build.sh
+++ debian/helpers/build.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 set -ex
-echo Installing Ubuntu $UBUNTU_RELEASE
+echo Installing Debian $DEBIAN_RELEASE
 export DEBIAN_FRONTEND=noninteractive
+
 apt-get update
 apt-get install -yq apt-utils
-yes | unminimize
 apt-get install -yq \
     systemd-sysv \
     vim \
@@ -24,24 +24,14 @@ apt-get -qq clean
 apt-get -qq autoremove
 
 # disable services we do not need
-systemctl disable systemd-resolved fstrim.timer fstrim
-if [ ${UBUNTU_RELEASE} = "20.04" ]; then
-    systemctl disable e2scrub_reap e2scrub_all e2scrub_all.timer
-    # systemd does not seem to realize that /dev/null is NOT a terminal
-    # under lx but when trying to chown it, it fails and thus the `User=`
-    # directive does not work properly ... this little trick fixes the
-    # behavior for the user@.service but obviously it has to be fixed in
-    # lx :) ...
-    touch /etc/systemd/null
-    mkdir -p /etc/systemd/system/user@.service.d
-    echo "[Service]\nStandardInput=file:/etc/systemd/null\n" \
-        > /etc/systemd/system/user@.service.d/override.conf
-fi
+systemctl disable \
+    systemd-resolved fstrim.timer fstrim \
+    e2scrub_reap e2scrub_all e2scrub_all.timer
 
 # disable systemd features not present in lx (e.g. cgroup support)
 for S in \
-    systemd-hostnamed systemd-localed systemd-timedated systemd-logind \
-    systemd-initctl systemd-journald
+    systemd-hostnamed systemd-localed systemd-timedated \
+    systemd-logind systemd-initctl systemd-journald
 do
     O=/etc/systemd/system/${S}.service.d
     mkdir -p $O
@@ -49,7 +39,6 @@ do
 done
 
 # Prevents apt-get upgrade issue when upgrading in a container environment.
-# Similar to https://bugs.launchpad.net/ubuntu/+source/makedev/+bug/1675163
 cp makedev /etc/apt/preferences.d/makedev
 cp locale.conf /etc/locale.conf
 cp locale /etc/default/locale
@@ -64,10 +53,6 @@ systemctl enable regenerate_ssh_host_keys
 cp create_hosts_file.service /etc/systemd/system
 systemctl enable create_hosts_file.service
 
-# Remove the divert that disables services
-rm -f /sbin/initctl
-dpkg-divert --local --rename --remove /sbin/initctl
-
 # add dtrace tools
 curl -sSLO https://mirrors.omnios.org/lx/dtracetools-lx_1.0_amd64.deb
 dpkg -i dtracetools-lx_1.0_amd64.deb
@@ -75,3 +60,4 @@ rm dtracetools-lx_1.0_amd64.deb
 
 # some smf helper folders
 mkdir -p /var/svc /var/db
+
